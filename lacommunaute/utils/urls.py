0e5b36b3e178b1b636a46b5930efbff9e87021db
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-11-18 20:41:16 +0100

    utils: Add add_url_params function
    copy paste from les-emplois

--- lacommunaute/utils/urls.py
+++ lacommunaute/utils/urls.py
@@ -1,9 +1,10 @@
 import re
+from urllib.parse import parse_qsl, urlsplit
 
 from django.conf import settings
 from django.utils.functional import keep_lazy_text
 from django.utils.html import Urlizer as BaseUrlizer
-from django.utils.http import url_has_allowed_host_and_scheme
+from django.utils.http import url_has_allowed_host_and_scheme, urlencode
 from django.utils.regex_helper import _lazy_re_compile
 
 
@@ -68,3 +69,31 @@ def clean_next_url(url):
     if not url_has_allowed_host_and_scheme(url, allowed_hosts=settings.ALLOWED_HOSTS):
         return "/"
     return url
+
+
+def add_url_params(url: str, params: dict[str, str]) -> str:
+    """Add GET params to provided URL being aware of existing.
+
+    :param url: string of target URL
+    :param params: dict containing requested params to be added
+    :return: string with updated URL
+
+    >> url = 'http://localhost:8000/login/activate_employer_account?next_url=%2Finvitations
+    >> new_params = {'test': 'value' }
+    >> add_url_params(url, new_params)
+    'http://localhost:8000/login/activate_employer_account?next_url=%2Finvitations&test=value
+    """
+
+    # Remove params with None values
+    params = {key: params[key] for key in params if params[key] is not None}
+    try:
+        url_parts = urlsplit(url)
+    except ValueError:
+        # URL is invalid so it's useless to continue.
+        return None
+    query = dict(parse_qsl(url_parts.query))
+    query.update(params)
+
+    new_url = url_parts._replace(query=urlencode(query)).geturl()
+
+    return new_url
