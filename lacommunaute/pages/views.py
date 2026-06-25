2399954754bbb3b533c087d7c3ee2154303c8b39
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-06-25 14:03:32 +0200

    chore: Simpler home page
    Don't link to recently edited forums : we won't use forum soon enough
    No need for a modal for les emplois

--- lacommunaute/pages/views.py
+++ lacommunaute/pages/views.py
@@ -1,15 +1,8 @@
 import logging
-from typing import Any
 
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render
-from django.utils import timezone
 from django.views.generic.base import TemplateView
-
-from lacommunaute.event.models import Event
-from lacommunaute.forum.models import Forum
-from lacommunaute.forum_conversation.forms import PostForm
-from lacommunaute.forum_conversation.models import Topic
 
 
 logger = logging.getLogger(__name__)
@@ -24,15 +17,6 @@ class LandingPagesListView(UserPassesTestMixin, TemplateView):
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["forums_category"] = Forum.objects.filter(parent__type=1).order_by("-updated")[:4]
-        context["forum"] = Forum.objects.get_main_forum()
-        context["upcoming_events"] = Event.objects.filter(date__gte=timezone.now()).order_by("date")[:4]
-        context["unanswered_topics"] = Topic.objects.unanswered()[:4]
-        context["form"] = PostForm(user=self.request.user)
-        return context
 
 
 def accessibilite(request):
--- tests/pages/test_homepage.py
+++ tests/pages/test_homepage.py
@@ -1,30 +1,14 @@
 import re
 
-import pytest
-from dateutil.relativedelta import relativedelta
 from django.urls import reverse
-from django.utils import timezone
 from itoutils.django.testing import assertSnapshotQueries
-from pytest_django.asserts import assertContains, assertNotContains
 
-from tests.event.factories import EventFactory
 from tests.forum.factories import CategoryForumFactory, ForumFactory
-from tests.forum_conversation.factories import TopicFactory
 from tests.testing import parse_response_to_soup
 
 
 def _sub_svg_suffix(content):
     return re.sub(r"\.\w+\.svg", ".svg", content)
-
-
-def test_context_data(client, db):
-    article = ForumFactory(parent=ForumFactory(type=1))
-    url = reverse("pages:home")
-
-    response = client.get(url)
-    assert response.status_code == 200
-
-    assert response.context_data["forums_category"].get() == article
 
 
 def test_page_title_header_footer(db, client, snapshot):
@@ -38,28 +22,6 @@ def test_page_title_header_footer(db, client, snapshot):
 
     footer = _sub_svg_suffix(str(parse_response_to_soup(response, selector="footer")))
     assert footer == snapshot(name="homepage_footer")
-
-
-def test_events(db, client):
-    old_event = EventFactory(date=timezone.now() - relativedelta(days=1))
-    visible_future_event = EventFactory.create_batch(4, date=timezone.now() + relativedelta(days=1))
-    unvisible_future_event = EventFactory(date=timezone.now() + relativedelta(days=1))
-    response = client.get(reverse("pages:home"))
-    assertContains(response, "Les prochains évènements", count=1)
-    assertNotContains(response, old_event.name)
-    for future_event in visible_future_event:
-        assertContains(response, future_event.name)
-        assertContains(response, reverse("event:detail", kwargs={"pk": future_event.pk}))
-    assertNotContains(response, unvisible_future_event.name)
-    assertContains(response, reverse("event:current"))
-
-
-@pytest.mark.parametrize("nb_topics,nb_expected", [(i, i) for i in range(5)] + [(i, 4) for i in range(5, 7)])
-def test_unsanswered_topics(db, client, nb_topics, nb_expected, snapshot):
-    TopicFactory.create_batch(nb_topics, with_post=True, with_tags=["Pirmadienis", "Poniedziałek", "Lundi", "Montag"])
-    response = client.get(reverse("pages:home"))
-    assert len(response.context_data["unanswered_topics"]) == nb_expected
-    assert ('id="unanswered_topics"' in str(response.content)) == (nb_expected > 0)
 
 
 def test_queries(db, client, snapshot):
