12f0d92cfc59a2f446a3a1b7454ae9ef4510e12a
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-06-26 16:31:39 +0200

    chore: Fixdev config
    Remove debug toolbar

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -28,26 +28,11 @@ CSRF_COOKIE_SECURE = False
 # Django-debug-toolbar.
 # ------------------------------------------------------------------------------
 
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-
 INTERNAL_IPS = ["127.0.0.1"]
 
 # Enable django-debug-toolbar with Docker.
 _, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS += [ip[:-1] + "1" for ip in ips]
-
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-
-DEBUG_TOOLBAR_CONFIG = {
-    # https://django-debug-toolbar.readthedocs.io/en/latest/panels.html#panels
-    "DISABLE_PANELS": [
-        "debug_toolbar.panels.redirects.RedirectsPanel",
-        # ProfilingPanel makes the django admin extremely slow...
-        "debug_toolbar.panels.profiling.ProfilingPanel",
-    ],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
-
 
 SECURE_CSP["default-src"] = ["*"]  # noqa undefined-local-with-import-star-usage
 SECURE_CSP["img-src"].append("localhost:9000")  # noqa undefined-local-with-import-star-usage
